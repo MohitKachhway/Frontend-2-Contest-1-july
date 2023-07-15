608b6176c4ef4ebb206e1cd026a310b9082fdbfd
MohitKachhway <mohitkachhway54@gmail.com>
2023-07-15 12:56:41 +0530

    submission

--- script.js
+++ script.js
@@ -8,20 +8,55 @@ let arr = [
 
 function PrintDeveloperbyMap() {
   //Write your code here , just console.log
+    let devWithMap=arr.map((srr)=>{
+      if(srr.profession=="developer")
+        return srr.name;
+     })
+    console.log(devWithMap);
 }
+
+
 
 function PrintDeveloperbyForEach() {
   //Write your code here , just console.log
+  arr.forEach((srr1)=>{
+    if(srr1.profession=="developer")
+    console.log(srr1.name);
+  })
 }
+
+
 
 function addData() {
   //Write your code here, just console.log
+  let obj={
+    id:4,
+    name:"susan",
+    age:20,
+    profession: "intern"
+  }
+  arr.push(obj);
+  console.log(arr);
 }
+
 
 function removeAdmin() {
   //Write your code here, just console.log
+  let removeObj=arr.filter((element)=>{
+    return element.profession!=="admin"
+      
+  })
+  console.log(removeObj)
 }
 
+ 
 function concatenateArray() {
   //Write your code here, just console.log
+  let arr2=[
+    { id: 4, name: "rocky", age: "34", profession: "teacher" },
+    { id: 5, name: "sam", age: "21", profession: "doctor" },
+    { id: 6, name: "romeo", age: "26", profession: "tester" }
+  ]
+  let concatArray=arr.concat(arr2)
+  console.log(concatArray)
 }
